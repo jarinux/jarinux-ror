0d6eae63cd6f60ac47004aba0cb106aa71e5a8a9
Antoine Nion <antoine@nion.fr>
2013-12-20 23:49:43 +0100

    Initialize depot with mysql database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Site::Application.config.secret_key_base = '039566db02c361c5df1ac8ed8711935145efa23083793890e55705f0d5be4c1240d755029fa8e3764256b5f398f5edd251cda6bfa9b3188a09f9a66296e27457'
